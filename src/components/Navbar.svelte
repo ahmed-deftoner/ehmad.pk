aa1ef2588f4e57de3dffd531d5e0665178ed6c80
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-20 19:24:27 +0500

    sveltey

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<nav class="bg-yellow-500 flex justify-between items-center p-4">
+    <div class="text-white mr-4">Ehmad Travels</div>
+    <div class="hidden justify-center items-center lg:flex">
+        <a href="/" class="text-white mr-10">Home</a>
+        <a href="/" class="text-white mr-10">Hajj</a>
+        <a href="/" class="text-white mr-10">Umrah</a>
+        <a href="/" class="text-white mr-10">About</a>
+        <a href="/" class="text-white mr-24">Gallery</a>
+        <a href="/" class="px-4 py-2 bg-yellow-600 rounded-full border-2 text-white border-white">Contact</a>
+    </div>
+    <div class="flex lg:hidden">
+        <div class="space-y-2">
+            <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+            <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+            <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+        </div>
+    </div>
+</nav>
