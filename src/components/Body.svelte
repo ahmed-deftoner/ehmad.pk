1787253f5502474c265be4c389c871c5ae6f9ae7
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-22 13:08:21 +0500

    msg change

--- src/components/Body.svelte
+++ src/components/Body.svelte
@@ -1,4 +1,4 @@
-<section id="testimonials">
+<section id="testimonials" class="x bg-slate-100">
     <!-- Container to heading and testm blocks -->
     <div class="max-w-6xl px-5 mx-auto mt-32 text-center">
       <!-- Heading -->
@@ -6,15 +6,15 @@
         Message from the CEO
       </h2>
       <!-- Testimonials Container -->
-      <div class="flex flex-col mt-24 sm:flex-row md:space-x-6">
+      <div class="flex flex-col mt-24 sm:flex-row">
         <!-- Testimonial 1 -->
         <div
-          class="flex flex-row items-center p-6 space-y-6 rounded-lg bg-slate-200 md:w-1/3"
+          class="flex flex-col items-center rounded-lg sm:flex-row"
         >
-          <img src="/hamid.jpg" class="w-20 h-24 -ml-14 rounded-full" alt="" />
+        <img src="/hamid.png" class="shadow-2xl rounded-full max-w-full h-auto align-middle border-none" alt="" />
           <div class="flex flex-col">
             <h5 class="text-lg font-bold">Hamid Younas</h5>
-            <p class="text-sm text-slate-800">
+            <p class="text-sm mt-3 text-slate-800 px-24 items-start">
               Umrah & Hajj are only for Allah. Those who love Allah and have the capacity to spend in His way, they must perform Hajj & Umrah. I have vast experience in this field and my aim is to serve guests of Allah at my best. I myself accompany Hujjaj from the first day of Hajj till the last moment for providing expert guidance at all places and to ensure a comfortable journey.
             </p>
           </div>
