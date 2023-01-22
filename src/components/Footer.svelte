bae863118c7a41aff13c4710bec9916419009b50
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-01-23 00:38:54 +0500

    blur background

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -46,7 +46,7 @@
         </div>
       </div>
       <!-- List Container -->
-      <div class="flex justify-around space-x-32">
+      <div class="flex justify-around space-x-24">
         <div class="flex flex-col space-y-3 text-white">
           <a href="/" class="hover:text-brightRed">Home</a>
           <a href="/" class="hover:text-brightRed">Pricing</a>
@@ -61,82 +61,88 @@
       </div>
 
       <!-- Input Container -->
-      <div class="flex flex-col justify-between">
-        <form>
-          <h1 class="text-slate-200 font-extrabold text-5xl pb-8">Send a message</h1>
-          <div class="form-group mb-6">
-            <input type="text" class="form-control block
-            w-full
-            px-3
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding
-            border border-solid border-gray-300
-            rounded
-            transition
-            ease-in-out
-            m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
-              placeholder="Name">
+      <div class="flex flex-col justify-between mx-4">
+        <div class="relative">
+          <div class="absolute -inset-1 bg-gradient-to-r from-pink-600 to-purple-500 rounded-lg blur opacity-75 hover:opacity-100 
+          transition duration-600 hover:duration-200 animate-tilt">
           </div>
-          <div class="form-group mb-6">
-            <input type="email" class="form-control block
+          <form class="relative backdrop-blur-md bg-gray-900 rounded-lg p-5">
+            <h1 class="text-slate-200 font-extrabold text-5xl pb-8">Send a message</h1>
+            <div class="form-group mb-6">
+              <input type="text" class="form-control block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+                placeholder="Name">
+            </div>
+            <div class="form-group mb-6">
+              <input type="email" class="form-control block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+                placeholder="Email address">
+            </div>
+            <div class="form-group mb-6">
+              <textarea class="
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+            " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
+            </div>
+            <button type="submit" class="
             w-full
-            px-3
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding
-            border border-solid border-gray-300
+            px-6
+            py-2.5
+            bg-blue-600
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
             rounded
+            shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg
             transition
-            ease-in-out
-            m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
-              placeholder="Email address">
-          </div>
-          <div class="form-group mb-6">
-            <textarea class="
-            form-control
-            block
-            w-full
-            px-3
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding
-            border border-solid border-gray-300
-            rounded
-            transition
-            ease-in-out
-            m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-          " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
-          </div>
-          <button type="submit" class="
-          w-full
-          px-6
-          py-2.5
-          bg-blue-600
-          text-white
-          font-medium
-          text-xs
-          leading-tight
-          uppercase
-          rounded
-          shadow-md
-          hover:bg-blue-700 hover:shadow-lg
-          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-blue-800 active:shadow-lg
-          transition
-          duration-150
-          ease-in-out">Send</button>
-        </form>
-        <div class="hidden text-white md:block">
+            duration-150
+            ease-in-out">Send</button>
+          </form>
+        </div>
+       
+        <div class="hidden mt-5 text-white md:block">
           Copyright &copy; 2022, All Rights Reserved
         </div>
       </div>
