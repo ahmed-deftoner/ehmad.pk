a43b6fa3cb672845d771ac1c9f158b64c4235aba
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-03-08 23:23:15 +0500

    main

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,63 +1,46 @@
-<div>
-  <section
-    class="relative flex bg-gray-900 flex-col-reverse sm:flex-row"
+<section
+  class="relative flex flex-col-reverse sm:flex-row"
+>
+  <div
+    class="absolute mx-auto max-w-screen-xl px-4 py-14 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
   >
-    <div
-      class="relative mx-auto max-w-screen-xl px-4 py-14 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
-    >
-      <div class="max-w-xl text-center sm:text-left">
-        <h1 class="font-extrabold text-5xl sm:text-5xl text-white">
-          HAJJ
+    <div class="max-w-xl rounded-lg backdrop-blur-2xl p-4 text-center sm:text-left">
+      <h1 class="font-extrabold text-5xl sm:text-5xl text-slate-100">
+        HAJJ
 
-          <strong class="block font-extrabold text-yellow-500">
-            2023
-          </strong>
-        </h1>
+        <strong class="block font-extrabold text-yellow-500">
+          2023
+        </strong>
+      </h1>
 
-        <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed text-white">
-          Your five star travel partner <br>
-          for Hajj and Ummrah
-        </p>
-          <a
-            href="/"
-            class="block mt-8 flex-wrap gap-4 text-center w-full rounded bg-yellow-500 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
-          >
-            View Packages
-          </a>
-      </div>
+      <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed text-slate-100">
+        Your five star travel partner <br>
+        for Hajj and Ummrah
+      </p>
+        <a
+          href="/"
+          class="block mt-8 flex-wrap gap-4 text-center w-full rounded bg-yellow-500 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+        >
+          View Packages
+        </a>
     </div>
-    <img class="my-3 p-10 container max-h-screen max-w-md sm:max-w-xl" src="/kaaba.png" alt="kaaba"/>
-  </section>
-  <section class="bg-slate-100 top-0"> 
-    <div class="custom-shape-divider-top-1674307116">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-      </svg>
-    </div>
-  </section>
-</div>
+  </div>
+  <img class="w-screen" src="/kaaba1.jpg" alt="kaaba"/>
+  <div class="backe"></div>
+</section>
+
 
 
 <style>
-  .custom-shape-divider-top-1674307116 {
-    position: absolute;
-    top: 100;
-    left: 0;
-    width: 100%;
-    overflow: hidden;
-    line-height: 0;
-}
 
-.custom-shape-divider-top-1674307116 svg {
-    position: relative;
-    display: block;
-    width: calc(100% + 1.3px);
-    height: 115px;
-    background-color: rgb(241, 245, 249);
-}
-
-.custom-shape-divider-top-1674307116 .shape-fill {
-    fill: rgb(17,24,39);
-}
-
+  .backe {
+    position:absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    z-index:2;
+    background: rgb(241,245,249);
+background: linear-gradient(180deg, rgba(241,245,249,0) 0%, rgba(241,245,249,0.12386204481792717) 75%, rgba(241,245,249,1) 100%);
+  }
 </style>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,18 +1,18 @@
-<nav class="flex bg-maindark justify-between items-center p-4">
-    <img src="/logo.png" alt="logo" class="h-[2.5rem] w-[3.5rem]"/>
+<nav class="flex bg-slate-100 justify-between items-center p-4">
+    <img src="/logo.webp" alt="logo" class="h-[3rem] w-[3.5rem]"/>
     <div class="hidden justify-center items-center lg:flex">
-        <a href="/" class="text-white px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-white hover:rounded-xl">Home</a>
-        <a href="/" class="text-white px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-white hover:rounded-xl">Hajj</a>
-        <a href="/" class="text-white px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-white hover:rounded-xl">Umrah</a>
-        <a href="/" class="text-white px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-white hover:rounded-xl">About</a>
-        <a href="/" class="text-white px-6 py-2 border-2 border-transparent transition duration-150 ml-1 mr-10 hover:ease-in hover:border-white hover:rounded-xl">Gallery</a>
-        <a href="/" class="px-6 py-1 bg-yellow-600 rounded-full border-2 text-white border-white focus:accent-slate-800">Contact</a>
+        <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Home</a>
+        <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Hajj</a>
+        <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Umrah</a>
+        <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">About</a>
+        <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 ml-1 mr-10 hover:ease-in hover:border-maindark hover:rounded-xl">Gallery</a>
+        <a href="/" class="px-6 py-1 bg-yellow-600 rounded-full border-2 text-slate-100 border-slate-100 focus:text-maindark">Contact</a>
     </div>
     <div class="flex lg:hidden">
         <div class="space-y-2">
-            <span class="block w-8 h-0.5 bg-slate-50 animate-pulse"></span>
-            <span class="block w-8 h-0.5 bg-slate-50 animate-pulse"></span>
-            <span class="block w-8 h-0.5 bg-slate-50 animate-pulse"></span>
+            <span class="block w-8 h-0.5 bg-maindark animate-pulse"></span>
+            <span class="block w-8 h-0.5 bg-maindark animate-pulse"></span>
+            <span class="block w-8 h-0.5 bg-maindark animate-pulse"></span>
         </div>
     </div>
 </nav>
