f905b232c90de29fb3f55e37a9c9943758665a61
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-03-09 14:31:10 +0500

    hambuerger

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,14 @@
 <script>
+    import { fly, scale } from 'svelte/transition';
+    import { quadOut } from 'svelte/easing';
+
     let drop = false;
+
+    let showMenu = false;
+
+    function toggleNavbar() {
+        showMenu = !showMenu;
+    }
 
     function toggle() {
         drop = !drop;
@@ -21,20 +30,52 @@
             {/if}
         </button>
         <ul class="p-2 absolute z-10 bottom-2/3 bg-slate-100 w-80 shadow bg-base-100 rounded-box" style:visibility={drop ? 'visible' : 'hidden'}>
-			<li><a href="/" class="text-maindark">Item 1</a></li>
-			<li><a href="/" class="text-maindark">Item 2</a></li>
-            <li><a href="/" class="text-maindark">Item 1</a></li>
-			<li><a href="/" class="text-maindark">Item 2</a></li>
+			<li><a href="/" class="text-maindark">Packages</a></li>
+			<li><a href="/" class="text-maindark">Facilities</a></li>
+            <li><a href="/" class="text-maindark">Training</a></li>
+			<li><a href="/" class="text-maindark">FAQs</a></li>
 		</ul>
         <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Umrah</a>
         <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 ml-1 mr-10 hover:ease-in hover:border-maindark hover:rounded-xl">Gallery</a>
         <a href="/" class="px-6 py-1 bg-yellow-600 rounded-full border-2 text-slate-100 border-slate-100 focus:text-maindark">Contact</a>
     </div>
-    <div class="flex lg:hidden">
-        <div class="space-y-2">
-            <span class="block w-8 h-0.5 bg-maindark animate-pulse"></span>
-            <span class="block w-8 h-0.5 bg-maindark animate-pulse"></span>
-            <span class="block w-8 h-0.5 bg-maindark animate-pulse"></span>
+    {#if showMenu === false}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <button on:click={toggleNavbar} class="md:hidden cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg"  fill="#000000" width="2rem" height="2rem" viewBox="0 0 1920 1920">
+                <path d="M1920 1468.412v112.94H0v-112.94h1920Zm0-564.706v112.941H0V903.706h1920ZM1920 339v112.941H0V339h1920Z" fill-rule="evenodd"/>
+            </svg>
+        </button>
+    {:else}
+        <div class="flex flex-col lg:hidden justify-center">
+
+            <button on:click={toggleNavbar} class="cursor-pointer self-end">
+                <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24" fill="none">
+                    <path d="M19 5L4.99998 19M5.00001 5L19 19" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+               </svg>
+            </button>
+            <div class="items-center md:hidden flex flex-col self-start">
+                <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Home</a>
+                <a href="/about" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">About</a>
+                <button on:click={toggle} class="flex flex-row pl-6 pr-4 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">
+                    <div class="text-maindark pr-4">Hajj</div>
+                    {#if drop === false}
+                        <svg class="self-end" xmlns="http://www.w3.org/2000/svg" height="1rem" width="1rem" viewBox="0 0 1024 1024" version="1.1"><path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" fill="#000000"/></svg>    
+                    {:else}
+                        <svg class="self-end" xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 1024 1024" version="1.1"><path d="M903.232 768l56.768-50.432L512 256l-448 461.568 56.768 50.432L512 364.928z" fill="#000000"/></svg>
+                        <ul style:visibility={drop ? 'visible' : 'hidden'}>
+                            <li><a href="/" class="text-maindark">Packages</a></li>
+                            <li><a href="/" class="text-maindark">Facilities</a></li>
+                            <li><a href="/" class="text-maindark">Training</a></li>
+                            <li><a href="/" class="text-maindark">FAQs</a></li>
+                        </ul>
+                    {/if}
+                </button>
+                <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Umrah</a>
+                <a href="/" class="text-maindark px-6 py-2 border-2 border-transparent transition duration-150 mx-1 hover:ease-in hover:border-maindark hover:rounded-xl">Gallery</a>
+                <a href="/" class="px-6 py-1 bg-yellow-600 rounded-full border-2 text-slate-100 border-slate-100 focus:text-maindark">Contact</a>
+            </div>
         </div>
-    </div>
+    {/if}
+    
 </nav>
